0fb1be2dab4c61df3fb03443072de24635da0390
Dmitry Smirnov <dmitry.smirnov@sap.com>
2021-01-17 12:04:20 +0100

    Add Date

--- models-layout/src/main/java/domain/Invoice.java
+++ models-layout/src/main/java/domain/Invoice.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.time.LocalDate;
 
 @Data
 @Entity
@@ -12,4 +13,5 @@ public class Invoice {
     @Id
     private Long id;
     private String Number;
+    private LocalDate Date;
 }
